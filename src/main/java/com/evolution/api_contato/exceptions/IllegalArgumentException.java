4089656fdceaeed13b3f83db71f2408af9309b8a
Sandro Muniz <sandrojam@hotmail.com>
2024-09-09 22:52:02 -0300

    Implementações das exceções (exceptions)
    Mecanismo especial para o tratamento de erros que possam ocorrer em tempo de execução da aplicação.

---
+++ src/main/java/com/evolution/api_contato/exceptions/IllegalArgumentException.java
@@ -0,0 +1,9 @@
+package com.evolution.api_contato.exceptions;
+
+public class IllegalArgumentException extends RuntimeException {
+
+    public IllegalArgumentException(String message){
+        super(message);
+    }
+
+}
